f98863f7985aa242d35f6d533cb277679221b4f0
VariousMilkshakes <vm@hadesoft.io>
2017-09-13 10:56:08 +0100

    Added basic overboard.log reading

---
+++ overboard-cli.js
@@ -0,0 +1,25 @@
+const fs		= require('fs');
+const overboard	= require('../lib/flare')("", "");
+
+let flares = [];
+
+fs.readFile(__dirname + '/overboard.log', 'utf-8', (err, data) => {
+	if (err) {
+		console.log("Could not open 'station.log'");
+		process.exit(0);
+	}
+
+	console.log(data);
+	parse(data);
+});
+
+function parse(data) {
+	rows = data.split('\n');
+
+	for (var i = 0; i < rows.length; i++) {
+		if (rows[i] != "")
+			flares[i] = flare.build(rows[i]);
+	}
+
+	console.log(i + " flares loaded");
+}
